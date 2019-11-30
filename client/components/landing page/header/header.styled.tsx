d4258336402843a98cf1e75597f2b5f8698362a3
NathanWaddell1211 <waddellnathan1211@gmail.com>
2019-11-29 22:48:13 -0600

    Starting header styling

--- client/components/landing page/header/header.styled.tsx
+++ client/components/landing page/header/header.styled.tsx
@@ -1,6 +1,30 @@
 import styled from "styled-components";
 
 export const HeaderOuterWrapper = styled.div`
-  margin: 0;
-  height: 90px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* background-color: #004380d9; */
+`;
+
+export const NineBallImage = styled.div`
+  width: 10%;
+  margin-left: 40px;
+  > img {
+    width: 100%;
+  }
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-right: 20px;
+  > a {
+    padding: 0 20px;
+    text-decoration: none;
+    color: #28bafd;
+    &:hover {
+      color: #028bca;
+    }
+  }
 `;
--- client/components/landing page/header/header.tsx
+++ client/components/landing page/header/header.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
 import * as Styled from "./header.styled";
+import { nineBallImage } from "../../../../static/img/9-ball-rack.jpg";
 
 const Header = () => {
   return (
     <Styled.HeaderOuterWrapper>
-      <h1>Header Component!!!!</h1>
+      <Styled.NineBallImage>
+        <img src={"static/img/9-ball-rack.png"} />
+      </Styled.NineBallImage>
+      <Styled.Buttons>
+        <a href='#'>Login</a>
+        <a href='#'>Signup</a>
+      </Styled.Buttons>
     </Styled.HeaderOuterWrapper>
   );
 };
