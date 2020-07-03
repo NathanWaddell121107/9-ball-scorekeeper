1c013e309a25df47a794707d08283a72288ec369
NathanWaddell1211 <waddellnathan1211@gmail.com>
2020-07-03 11:33:59 -0500

    Starting dashboard component setup

---
+++ client/components/landing page/dashboard/dashboard.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as Styled from "./dashboard.styled";
+
+const Dashboard = () => {
+  return <Styled.DashboardWrapper>Dashboard</Styled.DashboardWrapper>;
+};
+
+export default Dashboard;
