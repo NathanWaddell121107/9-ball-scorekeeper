5ed44a3583223edc0025932d55a862eda2265368
NathanWaddell1211 <waddellnathan1211@gmail.com>
2020-07-03 12:30:21 -0500

    Added dummy data to team stats

--- client/components/landing page/dashboard/team-stats/team-stats.styled.tsx
+++ client/components/landing page/dashboard/team-stats/team-stats.styled.tsx
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 
-export const TeamStatsWrapper = styled.div``;
-
-export const InnerWrapper = styled.div`
+export const TeamStatsWrapper = styled.div`
   padding: 20px;
   min-height: 400px;
   min-width: 500px;
@@ -13,5 +11,27 @@ export const InnerWrapper = styled.div`
     font-size: 1.25rem;
     font-weight: 500;
     text-align: center;
+    margin: 0.5rem;
+  }
+`;
+
+export const InnerWrapper = styled.div`
+  > p {
+    text-align: center;
+    font-size: 0.875rem;
+  }
+`;
+
+export const StatInfo = styled.div`
+  padding: 0 50px;
+  margin-top: 40px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  > div {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid black;
   }
 `;
--- client/components/landing page/dashboard/team-stats/team-stats.tsx
+++ client/components/landing page/dashboard/team-stats/team-stats.tsx
@@ -2,10 +2,34 @@ import * as React from "react";
 import * as Styled from "./team-stats.styled";
 
 const TeamStats = () => {
+  const updatedDate = "7/3/2020";
   return (
     <Styled.TeamStatsWrapper>
+      <h4>Team Stats</h4>
       <Styled.InnerWrapper>
-        <h4>Team Stats</h4>
+        <p>Last updated: {updatedDate}</p>
+        <Styled.StatInfo>
+          <div>
+            <p>Team Players: </p>
+            <p>5</p>
+          </div>
+          <div>
+            <p>Wins: </p>
+            <p>15</p>
+          </div>
+          <div>
+            <p>Losses: </p>
+            <p>2</p>
+          </div>
+          <div>
+            <p>Win %: </p>
+            <p>88%</p>
+          </div>
+          <div>
+            <p>Total Matches: </p>
+            <p>17</p>
+          </div>
+        </Styled.StatInfo>
       </Styled.InnerWrapper>
     </Styled.TeamStatsWrapper>
   );
