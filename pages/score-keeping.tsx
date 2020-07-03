d6d6a77d15f912cca125da50ae17ca52b39e80a8
NathanWaddell1211 <waddellnathan1211@gmail.com>
2020-07-03 13:01:21 -0500

    Adding score-keeping page

---
+++ pages/score-keeping.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "../client/components/layout/layout";
+import Header from "../client/components/landing page/header";
+
+const ScoreKeepingPage = () => {
+  return (
+    <Layout>
+      <Header />
+    </Layout>
+  );
+};
+
+export default ScoreKeepingPage;
