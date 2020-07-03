65ee96455976dc5be7675b7e8852d959a937c4b0
NathanWaddell1211 <waddellnathan1211@gmail.com>
2020-07-03 12:27:41 -0500

    Added dummy data to player stats

--- client/components/landing page/dashboard/player-stats/player-stats.tsx
+++ client/components/landing page/dashboard/player-stats/player-stats.tsx
@@ -2,10 +2,34 @@ import * as React from "react";
 import * as Styled from "./player-stats.styled";
 
 const PlayerStats = () => {
+  const updatedDate = "7/3/2020";
   return (
     <Styled.PlayerStatsWrapper>
+      <h4>Player Stats</h4>
       <Styled.InnerWrapper>
-        <h4>Player Stats</h4>
+        <p>Last updated: {updatedDate}</p>
+        <Styled.StatInfo>
+          <div>
+            <p>Skill Level: </p>
+            <p>7</p>
+          </div>
+          <div>
+            <p>Wins: </p>
+            <p>10</p>
+          </div>
+          <div>
+            <p>Losses: </p>
+            <p>1</p>
+          </div>
+          <div>
+            <p>Win %: </p>
+            <p>91%</p>
+          </div>
+          <div>
+            <p>Total Matches: </p>
+            <p>11</p>
+          </div>
+        </Styled.StatInfo>
       </Styled.InnerWrapper>
     </Styled.PlayerStatsWrapper>
   );
