9cab5071b83b91eb4f1951040992ef690acf98c5
NathanWaddell1211 <waddellnathan1211@gmail.com>
2020-07-10 22:20:02 -0500

    Setting up configuration files

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  printWidth: 180,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "always",
+  trailingComma: "es5",
+};
